d931acb264e452382975505db1c3c1dae1636f37
rafael.leroux <rafael.leroux@krypton.lan>
2026-05-25 13:02:15 -0400

    Created the Delete View and URl and added functionality

--- jokes/views.py
+++ jokes/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import CreateView,DetailView,ListView,UpdateView
+from django.views.generic import CreateView,DeleteView,DetailView,ListView,UpdateView
+from django.urls import reverse_lazy
 from .models import Joke
 
 # Create your views here.
@@ -6,6 +7,10 @@ class JokeCreateView(CreateView):
     model = Joke
     fields = ['question', 'answer']
 
+class JokeDeleteView(DeleteView):
+    model = Joke
+    success_url = reverse_lazy('jokes:list')
+    
 class JokeDetailView(DetailView):
     model = Joke
 
