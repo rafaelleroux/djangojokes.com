f67c48aefd5e214ef7732eea8ebdb4669b2b56e3
rafael.leroux <rafael.leroux@krypton.lan>
2026-05-25 12:17:27 -0400

    Creating Create and Update Views for the Jokes app and adding in the URLs

--- jokes/views.py
+++ jokes/views.py
@@ -1,10 +1,20 @@
-from django.views.generic import DetailView,ListView
+from django.views.generic import CreateView,DetailView,ListView,UpdateView
 from .models import Joke
 
 # Create your views here.
+class JokeCreateView(CreateView):
+    model = Joke
+    fields = ['question', 'answer']
+
 class JokeDetailView(DetailView):
     model = Joke
 
 class JokeListView(ListView):
     model = Joke
+
+class JokeUpdateView(UpdateView):
+    model = Joke
+    fields = ['question', 'answer']
+    
+
     
