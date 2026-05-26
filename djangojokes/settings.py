331b0197b34b8fbf4d47780b5028114758cc3e06
rafael.leroux <rafael.leroux@krypton.lan>
2026-05-26 18:38:48 -0400

    Messing around with Sendgrid and email

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,3 +133,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+if os.environ.get('ENVIRONMENT') != 'production':
+    from .local_settings import *
