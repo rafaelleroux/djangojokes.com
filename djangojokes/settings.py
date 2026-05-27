84fcba6a7043d5d7695dfd88577211ba3daafb0c
rafael.leroux <rafael.leroux@krypton.lan>
2026-05-26 21:36:27 -0400

    Adjusted email utility file, created a jobs app with a form that sends an email when filled out successfully, adjust settings and urls to reflect changes and created new templated under jobs

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     
     # Local Apps'
     'common.apps.CommonConfig',
+    'jobs.apps.JobsConfig',
     'jokes.apps.JokesConfig',
     'pages.apps.PagesConfig',
     'users.apps.UsersConfig',
