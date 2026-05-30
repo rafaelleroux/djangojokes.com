0b63121ef5ad7a38dc888831491593551adad93d
rafael.leroux <rafael.leroux@krypton.lan>
2026-05-30 00:03:57 -0400

    Adding a forms file in the jokes directory to create a ModelForm based on the default model and used it in the views.py file under jokes

--- jokes/views.py
+++ jokes/views.py
@@ -1,11 +1,12 @@
 from django.views.generic import CreateView,DeleteView,DetailView,ListView,UpdateView
 from django.urls import reverse_lazy
 from .models import Joke
+from .forms import JokeForm
 
 # Create your views here.
 class JokeCreateView(CreateView):
     model = Joke
-    fields = ['question', 'answer']
+    form_class = JokeForm
 
 class JokeDeleteView(DeleteView):
     model = Joke
@@ -19,7 +20,7 @@ class JokeListView(ListView):
 
 class JokeUpdateView(UpdateView):
     model = Joke
-    fields = ['question', 'answer']
+    form_class = JokeForm
     
 
     
