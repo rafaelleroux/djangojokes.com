79a32906bfec9aa6cfa68277c15d46e72240935e
rafael.leroux <rafael.leroux@krypton.lan>
2026-05-30 10:42:21 -0400

    Creating a tags model and updating existing modes for including Tags and organizing the options

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category,Joke
+from .models import Category,Joke, Tag
 
 # Register your models here.
 @admin.register(Joke)
@@ -23,3 +23,14 @@ class CategoryAdmin(admin.ModelAdmin):
         if obj: # editing an existing object
             return ('slug','created','updated')
         return()
+    
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
+    list_display = ['tag','created','updated']
+    
+    def get_readonly_fields(self, request, obj=None):
+        if obj: #editing an existing object
+            return ('slug', 'created', 'updated')
+        return()
+    
