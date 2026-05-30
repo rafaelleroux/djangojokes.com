5c540b0fe31eeff935b178fe59fa234897ad6c2c
rafael.leroux <rafael.leroux@krypton.lan>
2026-05-30 00:27:06 -0400

    Creating a new model for the Category field of a Joke form and also allowing to se eit on the Django Admin side

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Joke
+from .models import Category,Joke
 
 # Register your models here.
 @admin.register(Joke)
@@ -13,3 +13,13 @@ class JokeAdmin(admin.ModelAdmin):
             return ('slug','created', 'updated')
 
         return ()
+    
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = ['category','created','updated']
+    
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # editing an existing object
+            return ('slug','created','updated')
+        return()
