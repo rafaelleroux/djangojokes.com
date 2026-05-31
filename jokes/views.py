0eb65c5ec358804dcdd6c5291bcd1803cc964edc
rafael.leroux <rafael.leroux@krypton.lan>
2026-05-31 10:33:55 -0400

    Adding Success messages to the the Create and Update Joke views

--- jokes/views.py
+++ jokes/views.py
@@ -1,13 +1,15 @@
 from django.views.generic import CreateView,DeleteView,DetailView,ListView,UpdateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from .models import Joke
 from .forms import JokeForm
 
 # Create your views here.
-class JokeCreateView(LoginRequiredMixin,CreateView):
+class JokeCreateView(SuccessMessageMixin, LoginRequiredMixin, CreateView):
     model = Joke
     form_class = JokeForm
+    success_message = 'Joke created.'
     
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -27,9 +29,10 @@ class JokeDetailView(DetailView):
 class JokeListView(ListView):
     model = Joke
 
-class JokeUpdateView(UserPassesTestMixin,UpdateView):
+class JokeUpdateView(SuccessMessageMixin, UserPassesTestMixin, UpdateView):
     model = Joke
     form_class = JokeForm
+    success_message = 'Joke has been updated.'
     
     def test_func(self):
         obj = self.get_object()
