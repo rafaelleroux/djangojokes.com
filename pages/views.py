42da34ee7f4b37c17b5ccee24138ebb528153e90
rafael.leroux <rafael.leroux@krypton.lan>
2026-05-31 10:11:31 -0400

    Adding mesages and styling to the messages to see how they work, but commenting out the get method so it's not being displayed

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.views.generic import TemplateView
 
 # Create your views here.
@@ -6,3 +7,12 @@ class HomePageView(TemplateView):
     
 class AboutUsPageView(TemplateView):
     template_name = 'pages/about_us.html'
+    
+    '''def get(self, request, *args, **kwargs):
+        messages.debug(request, ' Debug message.')
+        messages.info(request, 'Info message.')
+        messages.success(request, 'Success message.')
+        messages.warning(request, 'Warning message.')
+        messages.error(request,'Error message.')
+        return super().get(request, args, kwargs)'''
+    
